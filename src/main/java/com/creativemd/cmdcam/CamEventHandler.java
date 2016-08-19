cd4196d098ce07aa2d3c84b68b87f60afced5045
CreativeMD <Creative-Multimedia-Design@gmx.de>
2016-08-19 11:31:00 +0200

    Readded camera roll

--- src/main/java/com/creativemd/cmdcam/CamEventHandler.java
+++ src/main/java/com/creativemd/cmdcam/CamEventHandler.java
@@ -2,11 +2,16 @@ package com.creativemd.cmdcam;
 
 
 import com.creativemd.cmdcam.key.KeyHandler;
+import com.creativemd.cmdcam.movement.OutsidePath;
 import com.creativemd.cmdcam.utils.CamPoint;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.EntityRenderer;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraftforge.client.event.EntityViewRenderEvent.CameraSetup;
+import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.RenderTickEvent;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
@@ -14,7 +19,7 @@ import net.minecraftforge.fml.relauncher.ReflectionHelper;
 public class CamEventHandler {
 
 	public static Minecraft mc = Minecraft.getMinecraft();
-	public static float defaultfov = mc.gameSettings.fovSetting;
+	public static float defaultfov = 70.0F;
 	public static final float amountZoom = 0.1F;
 	public static final float amountroll = 0.5F;
 	
@@ -69,7 +74,7 @@ public class CamEventHandler {
 					CMDCam.currentPath.tick(event.renderTickTime);
 				}
 				
-				ReflectionHelper.setPrivateValue(EntityRenderer.class, mc.entityRenderer, CMDCam.roll, "camRoll", "field_78495_O"); 
+				//ReflectionHelper.setPrivateValue(EntityRenderer.class, mc.entityRenderer, CMDCam.roll, "camRoll", "field_78495_O"); 
 				if(KeyHandler.startStop.isPressed())
 				{
 					if(CMDCam.currentPath != null)
@@ -84,6 +89,39 @@ public class CamEventHandler {
 				//mc.gameSettings.fovSetting = defaultfov;
 			//}
 		}
+	}
+	
+	public static boolean shouldPlayerTakeInput()
+	{
+		return true;
+	}
+	
+	public Entity renderEntity;
+	
+	@SubscribeEvent
+	public void renderPlayerPre(RenderPlayerEvent.Pre event)
+	{
+		if(CMDCam.currentPath instanceof OutsidePath)
+		{
+			renderEntity = mc.getRenderManager().renderViewEntity;
+			
+			mc.getRenderManager().renderViewEntity = mc.thePlayer;
+		}
+	}
+	
+	@SubscribeEvent
+	public void renderPlayerPost(RenderPlayerEvent.Post event)
+	{
+		if(CMDCam.currentPath instanceof OutsidePath)
+		{
+			mc.getRenderManager().renderViewEntity = renderEntity;
+		}
+	}
+	
+	@SubscribeEvent
+	public void cameraRoll(CameraSetup event)
+	{
+		event.setRoll(CMDCam.roll);
 	}
 	
 }
