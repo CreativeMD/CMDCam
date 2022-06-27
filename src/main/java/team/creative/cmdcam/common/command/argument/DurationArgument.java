03ed5aba183599ea8aee895e10b76f4c8e1ff5f3
CreativeMD <Creative-Multimedia-Design@gmx.de>
2022-06-27 18:19:27 +0200

    Fixed duration argument parsing fail message

--- src/main/java/team/creative/cmdcam/common/command/argument/DurationArgument.java
+++ src/main/java/team/creative/cmdcam/common/command/argument/DurationArgument.java
@@ -43,7 +43,7 @@ public class DurationArgument implements ArgumentType<Long> {
             factor = 1000 * 60 * 60 * 24;
         else {
             reader.setCursor(start);
-            throw new CommandSyntaxException(new SimpleCommandExceptionType(new LiteralMessage("Invalid time format")), Component.translatable("invalid_mode"));
+            throw new CommandSyntaxException(new SimpleCommandExceptionType(new LiteralMessage("Invalid time format")), Component.translatable("invalid_time_format"));
         }
         
         return time * factor;
