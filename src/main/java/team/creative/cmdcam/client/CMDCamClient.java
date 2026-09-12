1005f7a2a132fb95a4999b8243787c4e96d8441f
CreativeMD <Creative-Multimedia-Design@gmx.de>
2026-09-12 09:59:03 +0200

    Fixed /cam follow_center command

--- src/main/java/team/creative/cmdcam/client/CMDCamClient.java
+++ src/main/java/team/creative/cmdcam/client/CMDCamClient.java
@@ -44,7 +44,6 @@ public class CMDCamClient {
     private static CamScene playing;
     private static boolean serverAvailable = false;
     private static boolean hideGuiCache;
-    private static boolean hasTargetMarker;
     private static CamPoint targetMarker;
     
     public static CamPoint createLocalPoint() {
@@ -165,7 +164,7 @@ public class CMDCamClient {
                 x.getSource().sendFailure(Component.translatable(e.getMessage()));
             }
             return 0;
-        }))).then(new PointArgumentBuilder("follow_center", (x, y) -> targetMarker = y, PROCESSOR).executes(x -> {
+        }))).then(new PointArgumentBuilder("follow_center", (x, y) -> targetMarker = y, PROCESSOR, true).executes(x -> {
             targetMarker = createLocalPoint();
             return 0;
         })));
@@ -202,8 +201,7 @@ public class CMDCamClient {
     }
     
     public static void checkTargetMarker() {
-        hasTargetMarker = scene.posTarget != null;
-        if (hasTargetMarker && targetMarker == null)
+        if (scene.posTarget != null && targetMarker == null)
             targetMarker = createLocalPoint();
     }
     
@@ -274,7 +272,7 @@ public class CMDCamClient {
     }
     
     public static boolean hasTargetMarker() {
-        return hasTargetMarker && targetMarker != null && scene.posTarget != null;
+        return targetMarker != null && scene.posTarget != null;
     }
     
     public static CamPoint getTargetMarker() {
